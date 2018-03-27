f02e5396e96d68f3ddb82fc666a52914c03918f3
andrew <kdk0063@gmail.com>
2018-03-26 23:05:27 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='welcome'>
+        Hello Andrew! Let's Code! 
+      </div>
+    );
+  }
+}
+
+export default App;
